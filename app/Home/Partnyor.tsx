65dab3afe1f93c4ea6fd4c8d53133dfb6a2f2b17
ayxan65 <memmedliayxan0@gmail.com>
2024-06-03 20:47:38 +0400

    added

---
+++ app/Home/Partnyor.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Partnyor = () => {
+  return (
+    <div className='flex justify-center'>
+        <div className='w-11/12  h-72 '></div>
+    </div>
+  )
+}
+
+export default Partnyor
--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Link from 'next/link';
 import Avadanliq from './Avadanliq';
 import About from './About';
+import Partnyor from './Partnyor';
 
 const page = () => {
   return (
     <>
     <Avadanliq/>
     <About/>
+    <Partnyor/>
      
     </>
   )
