a999408f0ba8dc862c615232dc1e815d3a033256
ayxan65 <memmedliayxan0@gmail.com>
2024-06-03 21:33:41 +0400

    added

--- app/Home/Partnyor.tsx
+++ app/Home/Partnyor.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import Image from 'next/image';
+import vertiv from '../../public/partnyor/vertiv.svg';
+import glossel from '../../public/partnyor/glossel.svg';
+import deo from '../../public/partnyor/dea.svg';
+import delta from '../../public/partnyor/delta.svg';
+import auto from '../../public/partnyor/auto.svg';
 const Partnyor = () => {
   return (
     <div className='flex justify-center'>
-        <div className='w-11/12  h-72 '></div>
+        <div className='w-11/12  h-72 flex items-center justify-between '>
+            <div className='w-[30%]  text-4xl font-bold text-primary'>
+            Partnyorlar və
+            Müştərilər
+            </div>
+            <div>
+               <div className='flex justify-between  '> <Image src={vertiv} alt='vertiv'></Image>
+                <Image src={glossel} alt='glossel'></Image>
+                <Image src={deo} alt='deo'></Image>
+                <Image src={delta} alt='delta'></Image></div>
+                <div className='flex justify-between '>
+                <Image src={auto} alt='auto'></Image>
+                </div>
+
+
+            </div>
+        </div>
     </div>
   )
 }
