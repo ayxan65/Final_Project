24b50eadd34f30abf15172e850dcde4f164ad133
ayxan65 <memmedliayxan0@gmail.com>
2024-06-03 14:55:19 +0400

    added

--- app/Home/About.tsx
+++ app/Home/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import netpro from '../../public/Netprowhite.svg';
+import arrow from '../../public/icons/Aboutarrow.svg';
 
 
 const About = () => {
@@ -11,10 +12,15 @@ const About = () => {
 
 </Image></div>
   </div>
-  <div>
-    
-  </div>
+  <div className='flex flex-col items-center  justify-around h-96'>
+    <div className='text-white font-bold text-4xl my-7'>“NetPro Group” şirkəti haqqında</div>
+  
+  <div className='text-white  w-4/5  text-xl  flex justify-center leading-loose'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget ullamcorper ipsum mattis sed. Auctor proin in felis, nulla porta. Elementum, pharetra, egestas non vulputate eget odio elementum. Mauris felis nisi, consequat non diam egestas ac in diam. Arcu et risus tortor, sollicitudin. Ultrices sed ac lobortis at. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget ullamcorper ipsum mattis sed. Auctor proin in felis, nulla porta. Elementum, pharetra, egestas non vulputate eget odio elementum. Mauris felis nisi, consequat non diam egestas ac in diam. Arcu et risus tortor, sollicitudin. Ultrices sed ac lobortis at.</div>
 
+  <button className='w-36 h-12 bg-white rounded-lg text-primary flex justify-center items-center mt-5  '>Ətraflı
+  <Image className='ml-4' src={arrow} alt='arrow'></Image>
+  </button>
+  </div>
     </div>
   )
 }
