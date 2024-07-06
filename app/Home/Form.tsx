59ecdde600be149d05e220f5c77abd26d38fa469
ayxan65 <memmedliayxan0@gmail.com>
2024-07-06 13:23:46 +0400

    added

--- app/Home/Form.tsx
+++ app/Home/Form.tsx
@@ -1,8 +1,10 @@
-import { Nerko_One } from 'next/font/google'
+"use client"
+import Button from '@/components/Button.js'
 import React from 'react'
-import { BiRightArrow } from 'react-icons/bi'
+
 
 const Form = () => {
+
   return (
     <div className='bg-form'>
         <div className='flex justify-center '>
@@ -13,18 +15,13 @@ const Form = () => {
         </div>
           <div className='flex justify-center w-full mt-16'>
        <div className='flex flex-col'>
-       <input  placeholder='Adınız və Soyadınız' className=' p-3 form-input h-14 w-96 rounded-lg  ' type="text" />
-       <input  placeholder='Email ünvanı' className='p-3 form-input h-14 w-96 rounded-lg  mt-7' type="text" />
-       <input  placeholder='Əlaqə nömrəsi' className='p-3 form-input h-14 w-96 rounded-lg  mt-7' type="text" />
+       <input id='input1'  placeholder='Adınız və Soyadınız' className=' p-3 form-input h-14 w-96 rounded-lg  ' type="text" />
+       <input id='input2'  placeholder='Email ünvanı' className='p-3 form-input h-14 w-96 rounded-lg  mt-7' type="text" />
+       <input id='input3'  placeholder='Əlaqə nömrəsi' className='p-3 form-input h-14 w-96 rounded-lg  mt-7' type="text" />
        </div>
-       <textarea placeholder='Mesajınız' className='rounded-lg mx-7 p-3'   name="a" id="a"></textarea>
+       <textarea placeholder='Mesajınız' className='rounded-lg mx-7 p-3'   name="a" id="input4"></textarea>
           </div>
-       <div className='flex justify-center mt-8  mr-2'>
-        <button className='h-12 w-52 bg-primary text-white rounded-lg'>Göndər
-         
-        </button>
-
-       </div>
+      <Button/>
     </div>
   )
 }
