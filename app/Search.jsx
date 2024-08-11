d8767a49d2a4a30b02d8fd7b5e8f30bc8f6363de
ayxan65 <memmedliayxan0@gmail.com>
2024-08-11 18:33:22 +0400

    added

--- app/Search.jsx
+++ app/Search.jsx
@@ -14,40 +14,42 @@ const SearchBar = ({ data }) => {
 
   return (
     <div className="w-full max-w-md mx-auto mt-10">
-      <button
+ <div className='flex gap-2'>
+ <button
        
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <Image alt='search' src={search}></Image>
-       
-      </button>
+       onClick={() => setIsOpen(!isOpen)}
+     >
+       <Image alt='search' src={search}></Image>
+      
+     </button>
 
-      {isOpen && (
-        <div className="mt-4">
-          <input
-            type="text"
-            className="w-full p-2 border border-gray-300 rounded"
-            placeholder="Search..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {searchTerm && (
-            <ul className="">
-              {filteredData.length > 0 ? (
-                filteredData.map((item, index) => (
-                  <li key={index} className="p-2 border-b text-gray">
-                    <Link href={item.link}>
-                    {item.name}
-                    </Link>
-                  </li>
-                ))
-              ) : (
-                <li className="p-2 text-gray-500">No results found</li>
-              )}
-            </ul>
-          )}
-        </div>
-      )}
+     {isOpen && (
+       <div className="mt-4">
+         <input
+           type="text"
+           className="w-full p-2 border border-gray-300 rounded"
+           placeholder="Search..."
+           value={searchTerm}
+           onChange={(e) => setSearchTerm(e.target.value)}
+         />
+         {searchTerm && (
+           <ul className="">
+             {filteredData.length > 0 ? (
+               filteredData.map((item, index) => (
+                 <li key={index} className="p-2 border-b text-gray">
+                   <Link href={item.link}>
+                   {item.name}
+                   </Link>
+                 </li>
+               ))
+             ) : (
+               <li className="p-2 text-gray-500">No results found</li>
+             )}
+           </ul>
+         )}
+       </div>
+     )}
+ </div>
     </div>
   );
 };
